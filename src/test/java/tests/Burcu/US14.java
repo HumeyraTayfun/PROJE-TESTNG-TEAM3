7ea92efffd8769c02a3290ce27c2f88721a6dd7f
burcugocmen <Burcubaybars127@gmail.com>
2024-01-13 15:42:22 +0300

    13/01/2024 burcu

--- src/test/java/tests/Burcu/US14.java
+++ src/test/java/tests/Burcu/US14.java
@@ -1,4 +1,35 @@
 package tests.Burcu;
 
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import pages.user.registeredUser.BookingHistory;
+import pages.visitor.VisitorHomePage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
 public class US14 {
+    BookingHistory locator = new BookingHistory();
+
+    @Test
+    public void TestCase14(){
+        VisitorHomePage visitorHomePage=new VisitorHomePage();
+        SoftAssert softAssert1=new SoftAssert();
+
+
+        //1-User goes to the home page: https://qa.easybusticket.com/
+        Driver.getDriver().get(ConfigReader.getProperty("userUrl"));
+        //2-User must click on "Allow Cookies"button.
+        ReusableMethods.wait(2);
+        visitorHomePage.cookiesAllow.click();
+       // 3-click on the reservation history page from the reservation menu
+        ReusableMethods.userLogin(ConfigReader.getProperty("burcuUsername"),ConfigReader.getProperty("burcuPassword"));
+
+        //4-ticket information available on the reservation history page should be
+        //displayed(rezervazyon gecmısı sayfasında bulunan bilet bilgileri görüntülemelidir.)
+         locator.HeaderBooking.click();
+         locator.HeaderBookingHistory.click();
+         locator.HeaderRezervasyonGecmişi.isDisplayed();
+
+    }
 }
