c2d7f49ff0cd33c74d9c16c6bb7bef200cb4e2ec
Hümeyra Tayfun <humeyratayfun@hotmail.com>
2024-01-13 22:19:19 +0300

    13/01/2024 humeyra

--- src/test/java/pages/admin/paymentHistory/AllPayment.java
+++ src/test/java/pages/admin/paymentHistory/AllPayment.java
@@ -1,9 +1,21 @@
 package pages.admin.paymentHistory;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
 public class AllPayment {
     public AllPayment(){
-        PageFactory.initElements(Driver.getDriver(),this);}
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    @FindBy(xpath = "//*[text()='Payment History']")
+    public WebElement paymentHistoryLink;
+
+    @FindBy(xpath = "(//*[text()='All Payment'])[1]")
+    public WebElement allPaymentLink;
+
+
+
+
 }
